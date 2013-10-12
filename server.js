bc41e9f5f87b074c18d13cb5225d67951260291d
Isaac Cohen <icohen@leapmotion.com>
2013-10-12 14:25:39 -0700

    more ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var file = new(static.Server)();
 var app = http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(5000);
+}).listen(process.env.PORT || 5000)
 
 console.log('sss');
 var io = require('socket.io').listen(app);
